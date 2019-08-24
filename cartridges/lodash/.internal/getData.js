f2eaf9a4593a738f706d2f607b7325bb8a85f2d1
Thomas Theunen <thomas.theunen@forward.eu>
2019-08-24 16:08:41 +0200

    Test curry

---
+++ cartridges/lodash/.internal/getData.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var metaMap = require('./metaMap');
+var noop = require('../noop');
+
+/**
+ * Gets metadata for `func`.
+ *
+ * @private
+ * @param {Function} func The function to query.
+ * @returns {*} Returns the metadata for `func`.
+ */
+var getData = !metaMap ? noop : function (func) {
+    return metaMap.get(func);
+};
+
+module.exports = getData;
