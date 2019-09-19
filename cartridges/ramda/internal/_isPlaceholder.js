965c5bd42c5a230f7b545c5786e03da8b77018c6
Thomas Theunen <thomas.theunen@gmail.com>
2019-09-19 20:38:42 +0200

    Fix haspath for custom objects

--- cartridges/ramda/internal/_isPlaceholder.js
+++ cartridges/ramda/internal/_isPlaceholder.js
@@ -1,7 +1,13 @@
 'use strict';
 
 module.exports = function _isPlaceholder(a) {
-    return a != null &&
-         typeof a === 'object' &&
-         a['@@functional/placeholder'] === true;
+    if (a != null && typeof a === 'object') {
+        try {
+            return a['@@functional/placeholder'] === true; // A DW java object will raise an exception
+        } catch (e) {
+            return false;
+        }
+    } else {
+        return false;
+    }
 };
