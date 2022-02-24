b631e7276ab1ea5accdd1041cf59ad30ac8b4cc5
Thomas Theunen <thomas.theunen@forward.eu>
2022-02-24 21:55:56 +0100

    Add Javascript support

--- cartridges/jsPDF/main.js
+++ cartridges/jsPDF/main.js
@@ -1,5 +1,6 @@
 var jsPDF = require('./jsPDF');
 require('./plugins/addImage')(jsPDF.API);
 require('./plugins/total_pages')(jsPDF.API);
+require('./plugins/javascript')(jsPDF.API);
 
 module.exports = jsPDF;
